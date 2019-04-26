b376b8f6c5fe6dd34d662e1eaf19fc67ecb5e7f5
Axel <axel.bertrand2@etu.univ-lyon1.fr>
2019-04-26 11:46:31 +0200

    Implémentation composite js

--- 02-composite/kebab.js
+++ 02-composite/kebab.js
@@ -1,18 +1,24 @@
 class Component {
   constructor() {}
   operation() {}
-  add (component) {}
+  isVegetarian() {}
+  add(component) {}
 }
 
 class Leaf extends Component {
-  constructor(name) {
+  constructor(name, isVeget) {
     super()
     this.name = name
+    this.isVeget = isVeget
     console.log(`Leaf ${name} created`)
   }
 
-  operation (){
+  operation() {
     console.log(`operation -> ${this.name}`)
+  }
+
+  isVegetarian() {
+    return this.isVeget
   }
 }
 
@@ -26,9 +32,11 @@ class Composite extends Component {
 
   operation() {
     console.log('Composite Operation for: ' + this.name)
-    this.children.forEach(element => {
-      element.operation()
-    })
+    this.children.forEach(element => element.operation())
+  }
+
+  isVegetarian() {
+    return this.children.every(element => element.isVegetarian())
   }
 
   add(component) {
@@ -41,11 +49,18 @@ let sandwich = new Composite('pain')
 
 let garniture = new Composite('garniture')
 
-garniture.add(new Leaf('salade'))
-garniture.add(new Leaf('tomate'))
-garniture.add(new Leaf('oignons'))
-//garniture.add(new Leaf('veau'))
-garniture.add(new Leaf('steak soja'))
+garniture.add(new Leaf('salade', true))
+garniture.add(new Leaf('tomate', true))
+garniture.add(new Leaf('oignons', true))
+//garniture.add(new Leaf('veau', false))
+garniture.add(new Leaf('steak soja', true))
 
 sandwich.add(garniture)
 sandwich.operation()
+
+if(sandwich.isVegetarian()) {
+  console.log("Le sandwich est végétarien")
+}
+else {
+  console.log("Le sandwich n'est pas végétarien")
+}
